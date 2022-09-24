35e09104bcf241bb97db1ba55e6ccb2e9a8b53fe
Pablo Baxter <pablobaxter@msn.com>
2022-09-23 17:57:34 -0700

    Add in live data library

---
+++ livedata/src/test/java/com/frybits/preferences/livedata/LiveDataPreferenceTest.kt
@@ -0,0 +1,97 @@
+package com.frybits.preferences.livedata
+
+import android.content.SharedPreferences
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.frybits.preferences.core.IntegerAdapter
+import com.frybits.preferences.core.Preference
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.toCollection
+import kotlinx.coroutines.launch
+import org.junit.Rule
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/*
+ *  Copyright 2022 Pablo Baxter
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * Created by Pablo Baxter (Github: pablobaxter)
+ * https://github.com/pablobaxter/rx-preferences
+ *
+ */
+
+class LiveDataPreferenceTest {
+
+    @get:Rule
+    val executorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun testPreferenceLiveDataEmitOnStart() {
+        val sharedPrefs = mock<SharedPreferences> {
+            on { getInt(any(), any()) } doReturn 2
+        }
+        val keysChangedLiveData = MutableLiveData<String?>()
+        val liveDataPref = Preference(sharedPrefs, "test", -1, IntegerAdapter).asLiveDataPreference(keysChangedLiveData)
+
+        val testResult = arrayListOf<Int>()
+        liveDataPref.asLiveData().observeForever { testResult.add(it) }
+
+        // Ensure value is emitted on start
+        assertEquals(1, testResult.size)
+        assertEquals(2, testResult.first())
+        verify(sharedPrefs, times(1)).getInt(eq("test"), eq(-1))
+    }
+
+    @Test
+    fun testPreferenceLiveDataNoEmit() {
+        val sharedPrefs = mock<SharedPreferences> {
+            on { getInt(any(), any()) } doReturn 2
+        }
+        val keysChangedLiveData = MutableLiveData<String?>()
+        val liveDataPref = Preference(sharedPrefs, "test", -1, IntegerAdapter).asLiveDataPreference(keysChangedLiveData)
+
+        val testResult = arrayListOf<Int>()
+        liveDataPref.asLiveData().observeForever { testResult.add(it) }
+
+        // No value is emitted for other keys
+        keysChangedLiveData.value = "test2"
+        assertEquals(1, testResult.size) // OnStart already emitted at least once
+        verify(sharedPrefs, times(1)).getInt(eq("test"), eq(-1))
+    }
+
+    @Test
+    fun testPreferenceLiveDataEmitOnClear() {
+        val sharedPrefs = mock<SharedPreferences> {
+            on { getInt(any(), any()) } doReturn 2
+        }
+        val keysChangedLiveData = MutableLiveData<String?>()
+        val liveDataPref = Preference(sharedPrefs, "test", -1, IntegerAdapter).asLiveDataPreference(keysChangedLiveData)
+
+        val testResult = arrayListOf<Int>()
+        liveDataPref.asLiveData().observeForever { testResult.add(it) }
+
+        // Value is emitted for "clear" event
+        keysChangedLiveData.value = null
+        assertEquals(2, testResult.size)
+        verify(sharedPrefs, times(2)).getInt(eq("test"), eq(-1))
+    }
+}
