1bea35a4cd125e66e9eb0ebafe62def9ab1d73b2
Pablo Baxter <pablobaxter@msn.com>
2022-10-22 14:55:45 -0700

    Update android gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation(gradleApi())
     implementation(gradleKotlinDsl())
     implementation("com.android.tools.build:gradle:7.3.0")
-    implementation("com.android.tools.build:gradle-api:7.3.0")
+    implementation("com.android.tools.build:gradle-api:7.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10")
     implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.2")
